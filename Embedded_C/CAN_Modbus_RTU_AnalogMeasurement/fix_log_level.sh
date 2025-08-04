0d703e56e4cd8dfe8c8831ef59eaef3fe704bd0d
Rechard Nabo <rechardnabo@gmail.com>
2025-08-04 13:45:51 +0200

    addad compilation file

--- Embedded_C/CAN_Modbus_RTU_AnalogMeasurement/fix_log_level.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Simple script to replace all instances of current_log_level with log_level
-sed -i 's/current_log_level/log_level/g' Main.c
-echo "All instances of 'current_log_level' replaced with 'log_level'"
